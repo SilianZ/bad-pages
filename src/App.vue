f541ef961139d68d3bcfbb1482814b1d630ad8cb
SilianZ <silian_zheng@outlook.com>
2024-08-14 22:55:05 +0800

    fix: bug

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,30 @@
 import { ref } from 'vue';
 const button_text = ref('点击跳转到 GitHub 仓库')
 const onClick = () => {
-  button_text.value = '跳转中……'
+  button_text.value = "跳转中……"
 }
 </script>
 
 <template>
   <img src="/bg.svg" class="logo" alt="Vite logo" />
-  s<a href="https://github.com/SilianZ/bad-pages"><button @click="onClick">{{ button_text }}</button></a>
+  <a href="https://github.com/SilianZ/bad-pages"><button @click="onClick">{{ button_text }}</button></a>
 </template>
 
 <style scoped>
 .logo{
   height: auto;
   width: auto;
+}
+button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  color: #ffffff;
 }
 </style>
