db19d01f990e0d57c3484a813403e561852c9a49
SilianZ <silian_zheng@outlook.com>
2024-08-14 22:46:39 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const button_text = ref('点击跳转到 GitHub 仓库')
+const onClick = () => {
+  button_text.value = '跳转中……'
+}
+</script>
+
+<template>
+  <img src="/bg.svg" class="logo" alt="Vite logo" />
+  s<a href="https://github.com/SilianZ/bad-pages"><button @click="onClick">{{ button_text }}</button></a>
+</template>
+
+<style scoped>
+.logo{
+  height: auto;
+  width: auto;
+}
+</style>
